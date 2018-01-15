433260ed82088405e434186bce0e24bedc233814
Jesse David Peterson <jesdavpet@gmail.com>
2018-01-14 22:46:48 -0500

    Stub source index, and unit-test instrumentation.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/** @param {String} pathStrings @returns {Array<String>} */
+function pathify (pathStrings /* all other arguments ignored */) {
+  const pathString = Array.isArray(pathStrings) ? pathStrings[0] : pathStrings
+  const pathAccumulated = []
+
+  return pathAccumulated
+}
+
+module.exports = pathify
---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+const { expect } = require(`chai`)
+const pathify = require(`../src/index`)
+
+describe(`Tag pathify\`...\``, () => {
+  it(`should fail until test casess are written`, () => {
+    expect(true).to.be(false)
+  })
+})
